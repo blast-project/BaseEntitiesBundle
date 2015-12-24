76e29ec3bcd2062d1f43e315cd131d9484587c5c
Baptiste SIMON <beta@e-glop.net>
2015-12-24 17:33:25 +0100

    Improving how table names are defined in Many-To-Many situations (FQDN)

--- EventListener/MappingListener.php
+++ EventListener/MappingListener.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\Id\UuidGenerator;
 use Doctrine\ORM\Mapping\ClassMetadata;
 use Doctrine\ORM\Event\LoadClassMetadataEventArgs;
 use Librinfo\BaseEntitiesBundle\EventListener\Traits\ClassChecker;
+use Doctrine\ORM\Mapping\ClassMetadataInfo;
 
 class MappingListener implements EventSubscriber
 {
@@ -34,21 +35,25 @@ class MappingListener implements EventSubscriber
             ->getConfiguration()
             ->getNamingStrategy();
 
-        if ($namingStrategy->classToTableName($metadata->getName()) == $metadata->table['name'])
+        // create a FQDN for the representing table
+        if ( $namingStrategy->classToTableName($metadata->getName()) == $metadata->table['name'] )
+            $metadata->table['name'] = $this->buildTableName($metadata->name);
+        
+        // create a FQDN for the ManyToMany induced tables
+        foreach ( $metadata->associationMappings as $field => $mapping )
+        if ( $mapping['type'] == ClassMetadataInfo::MANY_TO_MANY && $mapping['isOwningSide'] )
+        if ( $namingStrategy->classToTableName($mapping['joinTable']['name']) == $mapping['joinTable']['name'] )
         {
-            $newname = $metadata->name;
-            $newname = str_replace('Bundle\\Entity', '', $newname);
-            $newname = str_replace('\\', '_', $newname);
-            $newname = strtolower($newname);
-            $metadata->table['name'] = $newname;
+            $rc = new \ReflectionClass($mapping['targetEntity']);
+            $fqdn = $mapping['sourceEntity'].'__'.$rc->getShortName();
+            $metadata->associationMappings[$field]['joinTable']['name'] = $this->buildTableName($fqdn);
         }
-
+        
         $reflectionClass = $metadata->getReflectionClass();
 
-        if (!$reflectionClass || !$this->hasTrait($reflectionClass, 'Librinfo\BaseEntitiesBundle\Entity\Traits\Idable')) {
+        // return if the current entity doesn't use Idable trait
+        if (!$reflectionClass || !$this->hasTrait($reflectionClass, 'Librinfo\BaseEntitiesBundle\Entity\Traits\Idable'))
             return;
-        } // return if current entity doesn't use Idable trait          
-        
 
         $metadata->mapField([
             'id'         => true,
@@ -57,5 +62,13 @@ class MappingListener implements EventSubscriber
             'columnName' => "id",
         ]);
         $metadata->setIdGenerator(new UuidGenerator());
+    }
+    
+    protected static function buildTableName($class)
+    {
+        $tableName = str_replace('Bundle\\Entity', '', $class);
+        $tableName = str_replace('\\', '_', $tableName);
+        $tableName = strtolower($tableName);
+        return $tableName;
     }
 }
