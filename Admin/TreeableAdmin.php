5c199908df0d026e81a291ac18431a1f8d0bd05b
Baptiste SIMON <bs-public@e-glop.net>
2015-12-25 14:43:03 +0100

    Adding a CoreAdmin file, for Treeable stuff

---
+++ Admin/TreeableAdmin.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Librinfo\BaseEntitiesBundle\Admin;
+
+use Librinfo\CoreBundle\Admin\CoreAdmin;
+
+abstract class TreeableAdmin extends CoreAdmin
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function create($object)
+    {
+
+        $em = $this->getConfigurationPool()->getContainer()->get('doctrine')->getManager();
+
+        $this->prePersist($object);
+        foreach ($this->extensions as $extension)
+            $extension->prePersist($this, $object);
+
+        $object->setMaterializedPath('');
+        $em->persist($object);
+
+
+        if ($object->getParentNode() !== null)
+            $object->setChildNodeOf($object->getParentNode());
+        else
+            $object->setParentNode(null);
+
+        $object->setSortMaterializedPath($object->getRealMaterializedPath());
+
+        $this->postPersist($object);
+        foreach ($this->extensions as $extension)
+            $extension->postPersist($this, $object);
+
+        $em->flush();
+        $this->createObjectSecurity($object);
+
+        return $object;
+    }
+}
+
