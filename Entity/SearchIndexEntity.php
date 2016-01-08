a59977791bced62d131348ed207ac0a57d1881fd
Marcos BDM <marcos.bezerra@libre-informatique.fr>
2016-01-08 17:07:34 +0100

    added getters and setters to SearchIndexEntity

--- Entity/SearchIndexEntity.php
+++ Entity/SearchIndexEntity.php
@@ -21,7 +21,61 @@ abstract class SearchIndexEntity
 
 
     /**
-     * @var string
+     * @var object Entity
      */
     protected $object;
+
+    /**
+     * @param string $field
+     * @return self
+     */
+    public function setField($field)
+    {
+        $this->field = $field;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getField()
+    {
+        return $this->field;
+    }
+
+    /**
+     * @param string $keyword
+     * @return self
+     */
+    public function setKeyword($keyword)
+    {
+        $this->keyword = $keyword;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getKeyword()
+    {
+        return $this->keyword;
+    }
+
+    /**
+     * @param object $object
+     * @return self
+     */
+    public function setObject($object)
+    {
+        $this->object = $object;
+        return $this;
+    }
+
+    /**
+     * @return object
+     */
+    public function getObject()
+    {
+        return $this->object;
+    }
 }
