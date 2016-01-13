e9e176397e45866be7a742b1d133775961a22462
Marcos BDM <marcos.bezerra@libre-informatique.fr>
2016-01-13 18:00:43 +0100

    librinfo:update:search console command to batch-update the search indexes

--- Entity/Repository/SearchableRepository.php
+++ Entity/Repository/SearchableRepository.php
@@ -16,6 +16,16 @@ class SearchableRepository extends EntityRepository
         $this->_class      = $class;
     }
 
+    public function getSearchIndexClass()
+    {
+        return $this->getClassName() . 'SearchIndex';
+    }
+
+    public function getSearchIndexTable()
+    {
+        return $this->getEntityManager()->getClassMetadata($this->getSearchIndexClass())->getTableName();
+    }
+
     /**
      * Find the entities that have the appropriate keywords in their searchIndex
      *
@@ -38,7 +48,7 @@ class SearchableRepository extends EntityRepository
         {
             $subquery = $query->getEntityManager()->createQueryBuilder()
                 ->select("i$k.keyword")
-                ->from($this->getClassName() . 'SearchIndex', "i$k")
+                ->from($this->getSearchIndexClass(), "i$k")
                 ->where("i$k.object = o")
                 ->andWhere("i$k.keyword LIKE :search$k");
             $query->andWhere($query->expr()->exists( $subquery->getDql()));
@@ -48,5 +58,67 @@ class SearchableRepository extends EntityRepository
         //dump($query->getQuery()->getSQL());
         $results = $query->getQuery()->execute();
         return $results;
+    }
+
+    /**
+     * Does a batch update of the whole search index table
+     */
+    public function batchUpdate()
+    {
+        if (! $this->truncateTable() )
+            throw new \Exception('Could not truncate table ' . $this->getSearchIndexTable());
+
+        $em = $this->getEntityManager();
+        $indexClass = $this->getSearchIndexClass();
+
+        $batchSize = 100;
+        $offset = 0;
+        $query = $this->createQueryBuilder('o')
+            ->setMaxResults($batchSize);
+
+        do {
+            $query->setFirstResult($offset);
+            $entities = $query->getQuery()->execute();
+            foreach ( $entities as $entity )
+            {
+                $fields = $indexClass::$fields;
+                foreach ( $fields as $field )
+                {
+                    $keywords = $entity->analyseField($field);
+                    foreach ( $keywords as $keyword )
+                    {
+                        $index = new $indexClass;
+                        $index->setObject($entity);
+                        $index->setField($field);
+                        $index->setKeyword($keyword);
+                        $em->persist($index);
+                    }
+                }
+            }
+            $em->flush();
+            $offset += $batchSize;
+        }
+        while ( count($entities) > 0 );
+    }
+
+    /**
+     * Truncates the search index table
+     * @return boolean true if success
+     */
+    public function truncateTable()
+    {
+        $connection = $this->getEntityManager()->getConnection();
+        $dbPlatform = $connection->getDatabasePlatform();
+        $connection->beginTransaction();
+        try {
+            $q = $dbPlatform->getTruncateTableSql($this->getSearchIndexTable());
+            $connection->executeUpdate($q);
+            $connection->commit();
+            return true;
+        }
+        catch (\Exception $e) {
+            $connection->rollback();
+            return false;
+        }
     }
 }
