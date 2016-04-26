e2f42790e3605b0779759c11e3862c33635d3676
Marcos BDM <marcos.bezerra@libre-informatique.fr>
2016-04-26 17:58:57 +0200

    Added options (min/max node level, tree_query), translations, and corrections to the tree widget rendering in TreeableType

--- Form/Type/TreeableType.php
+++ Form/Type/TreeableType.php
@@ -11,6 +11,7 @@ use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Librinfo\BaseEntitiesBundle\Form\DataTransformer\ModelToIdTransformer;
 
 class TreeableType extends AbstractType
@@ -37,16 +38,17 @@ class TreeableType extends AbstractType
 
         $builder->resetViewTransformers();
         $builder->addViewTransformer(new ModelToIdTransformer($this->em, $options['class']), true);
-        
 
         $builder->addEventListener(FormEvents::POST_SET_DATA, function (FormEvent $event)
         {
             $form = $event->getForm();
-            $repo = $this->em->getRepository($form->getConfig()->getAttribute('data_collector/passed_options')['class']);
+            $passed_options = $form->getConfig()->getAttribute('data_collector/passed_options');
+            $repo = $this->em->getRepository($passed_options['class']);
+            $query = isset($passed_options['tree_query']) ? $passed_options['tree_query'] : 'getRootNodesWithTree';
 
             if ($repo instanceof TreeableRepositoryInterface)
             {
-                $this->choices = $this->buildTreeChoices($repo->getRootNodesWithTree());
+                $this->choices = $this->buildTreeChoices($repo->$query());
 
                 $form->getConfig()->getType()->getOptionsResolver()->setDefaults([
                     'choices' => $this->choices
@@ -60,14 +62,23 @@ class TreeableType extends AbstractType
 
     public function buildView(FormView $view, FormInterface $form, array $options)
     {
-        $choices = [];
+        $view->vars['choices'] = [];
 
-        foreach ($view->vars['choices'] as $choice)
-            $choices[] = $choice->data;
+        $object_id = ($view->vars['name'] == 'parentNode') ? $form->getParent()->getData()->getId() : null;
+        foreach ($this->choices as $choice) {
+            if ( $object_id && $choice->data->getId() == $object_id )
+                continue;
+            if ( $options['min_node_level'] && $choice->data->getNodeLevel() < $options['min_node_level'] )
+                continue;
+            if ( $options['max_node_level'] && $choice->data->getNodeLevel() > $options['max_node_level'] )
+                continue;
+            if ( $choice->data->isRootNode() ) {
+                $admin = $this->getAdmin($options);
+                $choice->label = $admin->trans('parent_root_node_label');
+            }
 
-        $choices = $this->choices;
-
-        $view->vars['choices'] = $choices;
+            $view->vars['choices'][] = $choice;
+        }
     }
 
     protected function buildTreeChoices($choices, $level = 0)
@@ -89,13 +100,48 @@ class TreeableType extends AbstractType
                     $this->buildTreeChoices($choice->getChildNodes(), $level + 1)
                 );
         }
-
         return $result;
     }
 
     protected function generateTreeSymbols($label, $level)
     {
         return str_repeat('- - ', $level) . $label;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'tree_query' => 'getRootNodesWithTree',
+            'min_node_level' => 0,
+            'max_node_level' => 0
+        ));
+    }
+
+
+    /**
+     * @param array $options
+     *
+     * @return FieldDescriptionInterface
+     *
+     * @throws \RuntimeException
+     */
+    protected function getFieldDescription(array $options)
+    {
+        if (!isset($options['sonata_field_description'])) {
+            throw new \RuntimeException('Please provide a valid `sonata_field_description` option');
+        }
+
+        return $options['sonata_field_description'];
+    }
+
+    /**
+     * @param array $options
+     *
+     * @return AdminInterface
+     */
+    protected function getAdmin(array $options)
+    {
+        return $this->getFieldDescription($options)->getAdmin();
     }
 
     public function getParent()
@@ -107,4 +153,4 @@ class TreeableType extends AbstractType
     {
         $this->em = $em;
     }
-    }
+ }
