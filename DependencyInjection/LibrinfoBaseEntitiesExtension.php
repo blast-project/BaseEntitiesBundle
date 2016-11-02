e60f4464963d37b6b4b7173ef98df9933afc96b0
Marcos BDM <marcos.bezerra@libre-informatique.fr>
2016-11-02 12:33:04 +0100

    Removed dependency to Knp Doctrine Behaviours

--- DependencyInjection/LibrinfoBaseEntitiesExtension.php
+++ DependencyInjection/LibrinfoBaseEntitiesExtension.php
@@ -20,15 +20,6 @@ class LibrinfoBaseEntitiesExtension extends LibrinfoCoreExtension
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
 
-        // Loading KnpDoctrineBehaviors services
-        // TODO : do we really need this ? (use treeable from Gedmo Doctrine Extensions ?)
-//        try
-//        {
-//            $KnpLoader = new Loader\YamlFileLoader($container, new FileLocator($container->getParameter('kernel.root_dir') . "/../vendor/knplabs/doctrine-behaviors/config/"));
-//            $KnpLoader->load('orm-services.yml');
-//        }
-//        catch (\Exception $e) { }
-
         $this->mergeParameter('librinfo', $container, __DIR__.'/../Resources/config');
     }
 }
