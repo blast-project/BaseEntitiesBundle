d5ad49bde32bc0e726f0bcbc0278bbc533e08e9e
Romain <romain.sanchez@libre-informatique.fr>
2016-11-22 16:32:49 +0100

    LibrinfoBaseEntitiesExtension now extends LibrinfoCoreExtension

--- DependencyInjection/LibrinfoBaseEntitiesExtension.php
+++ DependencyInjection/LibrinfoBaseEntitiesExtension.php
@@ -2,24 +2,8 @@
 
 namespace Librinfo\BaseEntitiesBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\Loader;
 use Librinfo\CoreBundle\DependencyInjection\LibrinfoCoreExtension;
 
 class LibrinfoBaseEntitiesExtension extends LibrinfoCoreExtension
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function load(array $configs, ContainerBuilder $container)
-    {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $loader->load('services.yml');
-
-        $this->mergeParameter('librinfo', $container, __DIR__.'/../Resources/config');
-    }
 }
