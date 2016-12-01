f33c888e3cea2b8a5860515e8eb2306b220fe7ff
Baptiste SIMON <beta@e-glop.net>
2016-12-01 18:03:48 +0100

    implementing the new BlastCoreExtension::load() method

--- DependencyInjection/LibrinfoBaseEntitiesExtension.php
+++ DependencyInjection/LibrinfoBaseEntitiesExtension.php
@@ -3,8 +3,6 @@
 namespace Librinfo\BaseEntitiesBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\Loader;
 use Blast\CoreBundle\DependencyInjection\BlastCoreExtension;
 
 class LibrinfoBaseEntitiesExtension extends BlastCoreExtension
@@ -15,14 +13,8 @@ class LibrinfoBaseEntitiesExtension extends BlastCoreExtension
     /**
      * {@inheritdoc}
      */
-    public function load(array $configs, ContainerBuilder $container)
+    public function loadListeners(ContainerBuilder $container, array $config)
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $loader->load('services.yml');
-
         $availableListeners = [
             'naming',
             'guidable',
@@ -77,6 +69,8 @@ class LibrinfoBaseEntitiesExtension extends BlastCoreExtension
                 ->setPublic(true)
                 ->addTag('kernel.event_subscriber');
         }
+        
+        return $this;
     }
 
     public function configValidate(ContainerBuilder $container)
